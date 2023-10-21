e2c94bef1eb27067f4f93dd47cc5e1cd4013026b
tres-caras <severinidiegoignacio@gmail.com>
2023-10-21 13:49:27 -0300

    removed motion

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -1,23 +1,7 @@
 import Footer from '../components/Footer';
 import React, { ReactNode } from 'react';
-import { motion } from 'framer-motion';
 import Header from '@/components/Header';
 
-const fadeInScale = {
-  hidden: {
-    opacity: 0,
-    scale: 0.8,
-    width: '-webkit-fill-available;'
-  },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 1
-    },
-    width: '-webkit-fill-available;'
-  }
-};
 
 type MainLayoutProps = {
   children: ReactNode
@@ -27,18 +11,15 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <div className='parallax-container'>
       <div className="main-container">
-      <picture>
-        <source media="(min-width: 736px)" srcSet="/FullColor_TransparentBg_1280x1024_72dpi.png" />
-        <source media="(max-width: 736px)" srcSet="/website_logo_transparent_background.png" />
-        <motion.img
-          className="image-on-banner"
-          alt="Logo Leas"
-          src="/website_logo_transparent_background.png"
-          initial="hidden"
-          animate="visible"
-          variants={fadeInScale}
-        />
-      </picture>
+        <picture>
+          <source media="(min-width: 736px)" srcSet="/FullColor_TransparentBg_1280x1024_72dpi.png" />
+          <source media="(max-width: 736px)" srcSet="/website_logo_transparent_background.png" />
+          <img
+            className="image-on-banner fadeInScale"
+            alt="Logo Leas"
+            src="/website_logo_transparent_background.png"
+          />
+        </picture>
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
